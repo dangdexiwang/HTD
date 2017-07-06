668311eef9e230055b4e4d6cbacb2170b2844c61
dangdexiwang <13469172206@sina.cn>
2017-07-06 10:56:39 +0800

    app
    app

---
+++ app/js/bx.js
@@ -0,0 +1,8 @@
+$(function()
+{
+	var nav=document.getElementsByClassName("nav");
+	var ul=nav[0].getElementsByTagName("ul");
+	var lis=ul[0].getElementsByTagName("li");
+	lis[1].style.background="deepskyblue";
+	lis[1].style.border="0.001rem solid deepskyblue";
+})
